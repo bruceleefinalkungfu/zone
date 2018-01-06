e471fff6a0fd302721082b1dbdf5693595f777aa
anurag.awasthi <anurag.awasthi@zycus.net>
2018-01-06 18:59:05 +0530

    hi

--- src/main/resources/static/js/lib/zin-utill.js
+++ src/main/resources/static/js/lib/zin-utill.js
@@ -2,10 +2,15 @@
 var app_name = "crudApp";
 var zin_css = {
 	title_bar_height : '20',
-	zin_window_boarder_radius : '5px', 
+	title_bar_opacity : 0.4,
+	 
 	position_absolute : 'position: absolute;',
 	border : 'border: thin solid black;',
+	
+	zin_window_boarder_radius : '5px',
+	zin_window_background_color : 'white',
 }
+var top_z_index = -1;
 
 function create_zin_window(is_just_opened){
 	var size = get_browser_resolution();
@@ -13,12 +18,13 @@ function create_zin_window(is_just_opened){
 	var width =  size.width/2; 
 	function get_title_bar(width){
 		return '<div class="zin-title-bar" style="width:'+width+'; height:'+zin_css.title_bar_height+'px;\
-		background-color:#0992e8; opacity: 0.3;\
+		background-color:#0992e8; opacity: '+zin_css.title_bar_opacity+';\
 		border-radius:'+zin_css.zin_window_boarder_radius+' '+zin_css.zin_window_boarder_radius+' 0px 0px; " ></div>';
 	}
 	
-	var prefix =  '<div class="zin-window" id='+get_random_alphanumeric(10)+' style="'+border+'\
-	width:'+width+'px; height:'+height+'px; border-radius: '+zin_css.zin_window_boarder_radius+';"';
+	var prefix =  '<div class="zin-window" id='+get_random_alphanumeric(10)+' style="'+zin_css.border+'\
+	width:'+width+'px; height:'+height+'px; border-radius: '+zin_css.zin_window_boarder_radius+';\
+	'+zin_css.position_absolute+' z-index: '+(++top_z_index)+'; background-color:'+zin_css.zin_window_background_color+';"';
 	var suffix = '>'+get_title_bar(width)+'<div>Made by a directive!</div></div>';
 	var index = 0;
 	var arr=[];
